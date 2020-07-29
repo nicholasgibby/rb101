897f1c52cd4a7ead4e803ca9bba1c5cd5e00eade
Nicholas Gibby <49326993+nicholasgibby@users.noreply.github.com>
2020-07-29 11:07:53 -0500

    Added topic 21

---
+++ lesson_2/topic_21/rock_paper_scissors.rb
@@ -0,0 +1,43 @@
+VALID_CHOICES = ['rock', 'paper', 'scissors']
+
+def prompt(message)
+  puts "=> #{message}"
+end
+
+def display_results(player, computer)
+  if (player == 'rock' && computer == 'scissors') ||
+     (player == 'paper' && computer == 'rock') ||
+     (player == 'scissors' && computer == 'paper')
+    prompt 'You won!'
+  elsif (player == 'rock' && computer == 'paper') ||
+        (player == 'paper' && computer == 'scissors')
+        (player == 'scissors' && computer == 'rock')
+    prompt 'Computer won!'
+  else
+    prompt "It's a tie!"
+  end
+end
+
+loop do
+  choice = ''
+  loop do
+    prompt "Choose one: #{VALID_CHOICES.join(', ')}"
+    choice = gets.chomp
+
+    break if VALID_CHOICES.include?(choice)
+
+    prompt "That's not a valid choice."
+  end
+
+  computer_choice = VALID_CHOICES.sample
+
+  prompt "You chose: #{choice}; Computer chose: #{computer_choice}"
+
+  display_results(choice, computer_choice)
+
+  prompt 'Do you want to play again?'
+  answer = gets.chomp
+  break unless answer.downcase.start_with? 'y'
+end
+
+prompt 'Thank you for playing. Goodbye!'
